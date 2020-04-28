86f81be87a0188844aae1256da915c7ee2669a5b
Craig Brunner <craigcbrunner@gmail.com>
2020-04-28 00:48:24 -0700

    Messing with text strokes...

--- src/scenes/hud/buy_panel.js
+++ src/scenes/hud/buy_panel.js
@@ -4,7 +4,7 @@ import businesses from '../../data/businesses';
 
 import { Viewport, Column } from 'phaser-ui-tools';
 const BUTTON_HEIGHT = 100;
-const BUTTON_WIDTH = 100;
+const BUTTON_WIDTH = constants.SIDE_PANEL_WIDTH;
 export default class BuyPanel extends Phaser.Scene
 {
 	constructor()
@@ -26,23 +26,51 @@ export default class BuyPanel extends Phaser.Scene
         const { name, basePrice, multiplier } = business;
         const spriteName = `${name}_image`;
 
-        //const buttonBackground = this.add.rectangle(0, 0, 100, 100, 0xfffff, 1);
-
+        // create the button container 
         const businessButtonContainer = this.add.container(constants.SIDE_PANEL_WIDTH / 2,yPos);
 
-        const buttonBackground = this.add.rectangle(0, 0, BUTTON_HEIGHT, BUTTON_WIDTH, 0xffffff, 0.2);
+        // rectangle behind the button
+        const buttonBackground = this.add.rectangle(0, 0, BUTTON_WIDTH, BUTTON_HEIGHT, 0xffffff, 0.2);
         businessButtonContainer.add(buttonBackground);
+
+        // button icon
         const businessIcon = this.add.sprite(0, 0, spriteName);
         businessIcon.setOrigin(0.5, 0.8)
-        businessIcon.displayWidth = BUTTON_WIDTH / 2;
-        businessIcon.displayHeight = BUTTON_HEIGHT / 2;
+        businessIcon.displayWidth = 50;
+        businessIcon.displayHeight = 50;
 
+        // title text 
+        const titleText = this.add.text(0, 0, name, { fill: '#000' })
+        .setInteractive();
+        titleText.setOrigin(0.5, 3)
+
+        // clickable buy button
+        const clickButton = this.add.text(0, 0, 'Buy', { font: "28px Arial Black", fill: '#0f0' })
+        .setInteractive();
+        clickButton.setStroke(0xde77ae, 12);
+        clickButton.strokeThickness = 32;
+        clickButton.setShadow(2, 2, "#333333", 2, false, true);
+
+        clickButton.setOrigin(0.5, -1.5)
+
+
+        clickButton.on('pointerdown', () => {
+            console.log('clicked!')
+        } )
+        clickButton.on('pointerover', () => {
+            clickButton.setStyle({ fill: '#ff0'});
+        } )
+        clickButton.on('pointerout', () => {
+            clickButton.setStyle({ fill: '#0f0' });
+        } );
+
+        // add everything to the container
         businessButtonContainer.add(businessIcon);
+        businessButtonContainer.add(clickButton);
+        businessButtonContainer.add(titleText);
 
 
-
-        console.log('what is business: ', name, basePrice, multiplier);
-
+        // return full button to add to view port
         return businessButtonContainer;
     }
 
@@ -63,5 +91,8 @@ export default class BuyPanel extends Phaser.Scene
         businesses.forEach((business) => {
           viewport.addNode(this.createBusinessBuyButton(business));
         });
+    }
+
+    update() {
     }
 }
